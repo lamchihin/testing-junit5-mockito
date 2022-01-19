48827950702742f93f115696d00d221f5097d9af
plam <smallshell@gmail.com>
2022-01-18 23:34:38 -0800

    Verify interactions with Mockito Mocks (org.mockito.Mockito.verify)

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -9,6 +9,11 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.atMost;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class SpecialitySDJpaServiceTest {
@@ -18,6 +23,17 @@ class SpecialitySDJpaServiceTest {
     @InjectMocks
     private SpecialitySDJpaService service;
 
+    @Test
+    void testDeleteById() {
+        service.deleteById(1L);
+        service.deleteById(1L);
+
+        verify(specialtyRepository, times(2)).deleteById(1L);
+        verify(specialtyRepository, atLeastOnce()).deleteById(1L);
+        verify(specialtyRepository, atMost(2)).deleteById(1L);
+        verify(specialtyRepository, never()).deleteById(5L);
+    }
+    
     @Test
     void testDelete() {
         service.delete(new Speciality());
