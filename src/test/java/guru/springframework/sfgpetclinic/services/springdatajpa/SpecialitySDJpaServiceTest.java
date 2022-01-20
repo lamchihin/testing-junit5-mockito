b158e5eb6347e5b2db69d927d6dbbf560cbf249b
plam <smallshell@gmail.com>
2022-01-19 23:08:09 -0800

    when...thenReturn

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -8,12 +8,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.atMost;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class SpecialitySDJpaServiceTest {
@@ -33,9 +36,17 @@ class SpecialitySDJpaServiceTest {
         verify(specialtyRepository, atMost(2)).deleteById(1L);
         verify(specialtyRepository, never()).deleteById(5L);
     }
-    
+
     @Test
     void testDelete() {
         service.delete(new Speciality());
+    }
+
+    @Test
+    void testFindById() {
+        when(specialtyRepository.findById(1L)).thenReturn(Optional.of(new Speciality()));
+        Speciality foundSpecialty = specialtyRepository.findById(1L).orElse(null);
+        assertThat(foundSpecialty).isNotNull();
+        verify(specialtyRepository).findById(1L);
     }
 }
